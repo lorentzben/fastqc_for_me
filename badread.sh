3dd37d11493b5f6324801ad3aed5cdb8fb0a1e85
Ben Lorentz <blorentz@luc.edu>
2019-12-13 10:59:55 -0600

    updated badread.sh with wgsim

--- badread.sh
+++ badread.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+sudo apt install python3.7-dev
+
 
 git clone https://github.com/rrwick/Badread.git
 pip3 install ./Badread
@@ -9,3 +11,13 @@ export PATH=$PATH:~/sratoolkit.2.9.6-1-ubuntu64/bin
 fastq-dump SRX3407690
 
 #TODO need to see how badread works with new genome, just make it messy 
+
+git clone https://github.com/lh3/wgsim.git
+cd wgsim 
+gcc -g -O2 -Wall -o wgsim wgsim.c -lz -lm
+export PATH=$PATH:~/sratoolkit.2.9.6-1-ubuntu64/bin
+fastq-dump SAMN01911278
+
+#these are bad qual good len and good qual good len 
+wgsim -e 0 -d 500 -1 153 -2 153 SAMN01911278.fastq good.read1.fq good.read2.fq /dev/null
+wgsim -e0.2 -d 500 -1 153 -2 153 SAMN01911278.fastq bad.read1.fq bad.read2.fq /dev/null
